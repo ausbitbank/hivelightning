2444955346c2f1ea3d0893acf084d76260dee40a
Brian <brian@podping.org>
2021-08-19 13:22:57 +0300

    clear the invoice field after a succesful send

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -167,7 +167,10 @@ export default {
     async sendKeychain (amount, token) {
       const user = ''
       const { success, msg, cancel, notInstalled, notActive } = await keychain(window, 'requestTransfer', user, this.to, parseFloat(amount).toFixed(3), this.invoice, token)
-      if (success) { this.$q.notify('Payment sent!') }
+      if (success) {
+        this.$q.notify('Payment sent!')
+        this.invoice = ''
+      }
       if (cancel) { this.$q.notify('Cancelled by user') }
       if (!cancel) { if (notActive) { this.$q.notify('Please allow keychain to access this website') } else if (notInstalled) { this.$q.notify('Keychain not available') } else { console.info(msg) } }
     },
