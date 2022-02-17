ee0701ff67d8fefbdc68f7f8996ff04691ac154a
Brian of London <brian@podping.org>
2022-02-17 11:50:30 +0200

    delete old files
    Signed-off-by: Brian of London <brian@podping.org>

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,7 @@ const routes = [
     ]
   },
   {
-    path: '/tally/',
+    path: '/gethive/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Tally.vue') }
