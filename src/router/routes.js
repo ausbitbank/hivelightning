2990bfcdde65b682d7dd7dbba737dce3cae9002c
Brian of London <brian@podping.org>
2022-02-17 11:54:59 +0200

    remove unused route

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,13 +7,6 @@ const routes = [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
-  {
-    path: '/gethive/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Tally.vue') }
-    ]
-  },
   // Always leave this as last one,
   // but you can also remove it
   {
