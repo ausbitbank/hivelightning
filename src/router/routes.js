656d5725b7fd7201faf6c5c7457d3a9928a621ea
Brian of London <brian@podping.org>
2022-03-08 14:36:12 +0200

    updated home page
    Signed-off-by: Brian of London <brian@podping.org>

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
+      { path: '/', component: () => import('pages/PageGetLightning.vue') },
       { path: '/lnd', component: () => import('pages/PageGetLightning.vue') },
       { path: '/hive', component: () => import('pages/PageGetHive.vue') },
       { path: '/info', component: () => import('pages/PageInfo.vue') }
